8645897c18de8e2eb30a6cd39a5d3ff5c48a33cf
k1n0trz <kinotrance@gmail.com>
2021-09-07 18:02:27 -0500

    varios

--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,17 @@
-// module.exports = {
-//     database: {
-//         host: 'us-cdbr-east-03.cleardb.com',
-//         user: 'be7135c7eb02c0',
-//         password: '57475ea8',
-//         database: 'heroku_cea3c0fd4f250a2'
-//     }
-// };
-
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'demo'
+        host: 'us-cdbr-east-03.cleardb.com',
+        user: 'be7135c7eb02c0',
+        password: '57475ea8',
+        database: 'heroku_cea3c0fd4f250a2'
     }
 };
+
+// module.exports = {
+//     database: {
+//         host: 'localhost',
+//         user: 'root',
+//         password: '',
+//         database: 'demo'
+//     }
+// };
